cd5707edea90cedab730d1e22ad3bec6538a9f4c
Mariano <salguero.ms@gmail.com>
2021-11-07 12:48:23 -0300

    :)

--- result.js
+++ result.js
@@ -9,33 +9,7 @@ async function decide() {
 
     document.getElementById("titleInput").textContent = title;
 
-    /* options.forEach(item => {
-         var li = document.createElement("li");
-
-         li.innerHTML = item;
-         //li.className = 'listItem';
-
-         document.getElementById("myUL").appendChild(li);
-     });*/
-
-    document.getElementById("textResult").textContent = "Esperando por el próximo bloque...";
-
-
-    myJson = await getBlockChainData();
-    var startHash = myJson.hash;
-    var myJson;
-
-    console.log(myJson.hash);
-    console.log(myJson.time);
-
-
-    do {
-        myJson = await getBlockChainData();
-        await sleep(1000);
-    } while (myJson.hash == startHash);
-
-    console.log(myJson.hash);
-    console.log(myJson.time);
+    const myJson = await getLastestEthBlockV2();
 
     let eth_hash = myJson.hash;
     let eth_hash_value = parseInt(eth_hash, 16);
@@ -59,6 +33,12 @@ async function decide() {
 }
 
 function print_results(options_results) {
+    var audio = document.getElementById('audio');
+    var source = document.getElementById('audioSource');
+    source.src = "celebration.ogg";
+    audio.load(); //call this to just preload the audio without playing
+    audio.play(); //call this to play the song right away
+
     options_results.sort((a, b) => {
         return a.dif - b.dif;
     });
@@ -68,13 +48,18 @@ function print_results(options_results) {
         var li = document.createElement("li");
 
         li.innerHTML = options_results[i].option;
-
+        li.classList = "result";
         document.getElementById("myUL").appendChild(li);
     }
 }
 
 async function getBlockChainData() {
-    const response = await fetch('https://api.blockcypher.com/v1/eth/main');
+    const response = await fetch('https://api.blockcypher.com/v1/eth/main', {
+        'mode': 'no-cors',
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+        }
+    });
     const myJson = await response.json(); //extract JSON from the http response
 
     return myJson;
@@ -82,4 +67,38 @@ async function getBlockChainData() {
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+
+async function getLastestEthBlock() {
+    myJson = await getBlockChainData();
+    var startHash = myJson.hash;
+    var myJson;
+
+    do {
+        myJson = await getBlockChainData();
+        await sleep(1000);
+    } while (myJson.hash == startHash);
+}
+
+async function getLastestEthBlockV2() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const lastBockNUmber = await provider.getBlockNumber();
+
+    let proxBlockNumber;
+    let i = 0;
+
+    do {
+        document.getElementById("textResult").textContent = "Esperando por el próximo bloque " + (i % 2 == 0 ? "☺" : "☻");
+        "(งツ)ว"
+        "(วツ)ง"
+        proxBlockNumber = await provider.getBlockNumber();
+        await sleep(1000);
+        i++;
+    } while (proxBlockNumber == lastBockNUmber);
+
+    const block = await provider.getBlock(proxBlockNumber);
+
+    return block.hash;
 }
